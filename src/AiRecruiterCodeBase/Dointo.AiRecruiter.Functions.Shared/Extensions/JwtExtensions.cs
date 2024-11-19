4510746f52a8969f5e60bcc224487b52b3dc4e63
Umair Tahir <theumairtahir@outlook.com>
2024-11-19 20:56:20 +0500

    Add new shared project and JWT handling methods
    Introduced `Dointo.AiRecruiter.Functions.Shared` project to the solution `AiRecruiterCodeBase.sln`, targeting `net9.0` with necessary package references. Configured build settings for Debug and Release modes. Added project reference to `Dointo.AiRecruiter.Core`.
    
    Included `JwtExtensions.cs` with methods for JWT token handling: `ValidateRequest`, `AuthorizeRequest`, `GetUserInfo`, and `GetJwt`. Added `HandleRequest.cs` with a static `Handle` method for processing HTTP requests and logging.

---
+++ src/AiRecruiterCodeBase/Dointo.AiRecruiter.Functions.Shared/Extensions/JwtExtensions.cs
@@ -0,0 +1,83 @@
+﻿using Dointo.AiRecruiter.Core.Extensions;
+using Dointo.AiRecruiter.Core.Params;
+using Dointo.AiRecruiter.Core.States;
+using Dointo.AiRecruiter.Core.Utils;
+using Microsoft.AspNetCore.Http;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Dointo.AiRecruiter.Functions.Shared.Extensions;
+
+public static class JwtExtensions
+{
+	public static string? ValidateRequest(this HttpRequest request, string? audienceId = null)
+	{
+		const string REQUEST_IS_NOT_AUTHORIZED_MESSAGE = "The request is not authorized";
+		const string TOKEN_EXPIRED = "The authentication token has been expired";
+
+		var jwt = GetJwt(request);
+		if (jwt is null)
+			return REQUEST_IS_NOT_AUTHORIZED_MESSAGE;
+
+		var utc = DateTime.UtcNow;
+		if (jwt.ValidFrom > utc || jwt.ValidTo < utc)
+			return TOKEN_EXPIRED;
+
+		if (audienceId is not null && jwt.Audiences.All(x => !x.IsEqual(audienceId)))
+			return REQUEST_IS_NOT_AUTHORIZED_MESSAGE;
+
+		return null;
+	}
+
+	public static IProcessingState AuthorizeRequest(this HttpRequest request, string? audienceId = null)
+	{
+		const string AUTHORIZED_MESSAGE = "The request has been authorized";
+
+		try
+		{
+			if (request.ValidateRequest(audienceId) is not string validationMessage)
+				return new SuccessState(AUTHORIZED_MESSAGE);
+			return new UnauthorizedState(validationMessage);
+		}
+		catch (Exception)
+		{
+			return new UnauthorizedState("Invalid token");
+		}
+
+	}
+
+	public static UserInfo? GetUserInfo(this HttpRequest request)
+	{
+		var jwt = GetJwt(request);
+		if (jwt is null)
+			return null;
+
+		var objectId = jwt.Claims.FirstOrDefault(x => x.Type.IsEqualIgnoreCase("oid"))?.Value ?? AppConstants.EMPTY_STRING;
+		var firstName = jwt.Claims.FirstOrDefault(x => x.Type.IsEqualIgnoreCase("given_name"))?.Value ?? AppConstants.EMPTY_STRING;
+		var lastName = jwt.Claims.FirstOrDefault(x => x.Type.IsEqualIgnoreCase("family_name"))?.Value ?? AppConstants.EMPTY_STRING;
+		var email = jwt.Claims.FirstOrDefault(x => x.Type.IsEqualIgnoreCase("emails"))?.Value ?? AppConstants.EMPTY_STRING;
+		var username = email.IsNullOrEmpty( ) ? firstName : email;
+		return new UserInfo(objectId, firstName, lastName, email, username);
+	}
+
+	private static JwtSecurityToken? GetJwt(HttpRequest request)
+	{
+		try
+		{
+			const string AUTH_HEADER_NAME = "Authorization";
+			const string BEARER_NAME = "Bearer";
+			if (request is null || request.Headers.All(x => !x.Key.IsEqualIgnoreCase(AUTH_HEADER_NAME)))
+				return null;
+			var header = request.Headers.First(x => x.Key.IsEqualIgnoreCase(AUTH_HEADER_NAME));
+			var token = header.Value.FirstOrDefault( );
+			if (token is null || !token.StartsWith(BEARER_NAME))
+				return null;
+
+			token = token.Replace(BEARER_NAME, string.Empty).Trim( );
+			return new JwtSecurityToken(token);
+		}
+		catch (Exception)
+		{
+			return null;
+		}
+	}
+}
---
+++ src/AiRecruiterCodeBase/Dointo.AiRecruiter.Functions.Shared/Handlers/HandleRequest.cs
@@ -0,0 +1,32 @@
+﻿using Dointo.AiRecruiter.Core.States;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Dointo.AiRecruiter.Functions.Shared.Handlers;
+public static class HandleRequest
+{
+	public static async Task<IActionResult> Handle([NotNull] Func<Task<IProcessingState>> actionAsync, string functionName, ILogger logger)
+	{
+		try
+		{
+			logger.LogInformation("Function Name: {FunctionName}, State: Entry", functionName);
+			var resultState = await actionAsync( );
+			logger.LogInformation("Function Name: {FunctionName}, State: Processed, Message: {Message}", functionName, resultState.Message);
+			if (resultState.Success)
+				return new OkObjectResult(resultState);
+			logger.LogWarning("Function Name: {FunctionName}, State: Error, Message: {Message}", functionName, resultState.Message);
+			return resultState switch
+			{
+				ErrorState errorState => new BadRequestObjectResult(errorState),
+				ValidationErrorState validationErrorState => new BadRequestObjectResult(validationErrorState),
+				_ => throw new InvalidOperationException("Not a valid state provided")
+			};
+		}
+		catch (Exception ex)
+		{
+			logger.LogCritical(ex, "Function Name: {FunctionName}, State: Exception", functionName);
+			return new BadRequestObjectResult(new ExceptionState($"An error occurred while running {functionName}", ex.Message));
+		}
+	}
+}

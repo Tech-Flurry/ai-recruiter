ba891cb0a95611cdc03b1616f306bb53299d46ee
Umair Tahir <theumairtahir@outlook.com>
2024-11-21 21:08:49 +0500

    Add project references and MongoDB configuration
    Updated `Dointo.AiRecruiter.RestApi.csproj` to include references to `AiInfrastructure`, `Application`, and `DbInfrastructure` projects. Added `using` statements and service registrations for `Application` and `DbInfrastructure` in `Program.cs`, with MongoDB settings for `DbInfrastructure`. Modified `appsettings.Development.json` and `appsettings.json` to include MongoDB connection settings.

--- src/AiRecruiterCodeBase/Dointo.AiRecruiter.RestApi/Program.cs
+++ src/AiRecruiterCodeBase/Dointo.AiRecruiter.RestApi/Program.cs
@@ -1,3 +1,6 @@
+using Dointo.AiRecruiter.Application;
+using Dointo.AiRecruiter.DbInfrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers( );
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi( );
+builder.Services.AddApplication( );
+builder.Services.AddDbInfrastructure("MongoDb:ConnectionString", "MongoDb:DatabaseName");
 
 var app = builder.Build( );
 

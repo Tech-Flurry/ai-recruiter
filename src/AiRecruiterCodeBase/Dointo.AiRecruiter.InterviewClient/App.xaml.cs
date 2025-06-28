a0e17fa4dc547923239e18c6650b5ba5d893aaf4
Umair Tahir <theumairtahir@outlook.com>
2025-06-28 14:52:22 +0500

    added interview app project

---
+++ src/AiRecruiterCodeBase/Dointo.AiRecruiter.InterviewClient/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Dointo.AiRecruiter.InterviewClient;
+
+public partial class App : Application
+{
+	public App( )
+	{
+		InitializeComponent( );
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage( )) { Title = "Dointo.AiRecruiter.InterviewClient" };
+	}
+}
---
+++ src/AiRecruiterCodeBase/Dointo.AiRecruiter.InterviewClient/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace Dointo.AiRecruiter.InterviewClient;
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp( ) => MauiProgram.CreateMauiApp( );
+}
